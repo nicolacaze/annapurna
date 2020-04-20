2ec4e3ece0034da57ef6678fb2e23f57ed951fd3
Nicolas <nicolas.lacaze@gmail.com>
2020-04-20 09:22:23 +0200

    Build homepage and grid style

--- src/components/image.js
+++ src/components/image.js
@@ -16,17 +16,17 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "rohit.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+          fluid(sizes: "100vw") {
+            src
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img style={{ height: '100%' }} fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Image
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,150 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
+import React, { useState } from "react"
+import Img from "gatsby-image"
+import styled from "styled-components"
 import SEO from "../components/seo"
+import Hero from "../components/image"
+import StoryPicture from "../images/cam-adams.jpg"
+
+import "../components/layout.css"
+
+const Page = styled.div`
+  display: grid;
+  grid-template-columns: 1fr [start-content] 600px [end-content] 1fr;
+  grid-template-rows: 100vh repeat(3, 1fr) 200px;
+  grid-gap: 2rem;
+  grid-template-areas:
+    "hero    hero    hero"
+    ".       content ."
+    ".       content ."
+    ".       content ."
+    "footer  footer  footer";
+  .hero {
+    grid-area: hero;
+  }
+  main {
+    grid-area: content;
+  }
+  footer {
+    grid-area: footer;
+  }
+`
+
+const Main = styled.main`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(2, minmax(200px, 1fr));
+  row-gap: 2rem;
+`
+
+const Card = styled.article`
+  grid-column: span 2;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.2s ease-out;
+  &:hover {
+    box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.1);
+    .content__title {
+      transform: translateY(0);
+    }
+    .content__text {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  .story__picture {
+    grid-column: 1 / 3;
+    height: 100%;
+    object-fit: cover;
+  }
+  .story__content {
+    grid-column: 3 / 5;
+    padding: 1rem;
+    h2 {
+      font-size: 1rem;
+    }
+    p {
+      font-size: 0.8rem;
+    }
+  }
+  .content__title {
+    transform: translateY(100%);
+    transition: all 0.5s ease-out;
+  }
+  .content__text {
+    transform: translateY(40px);
+    opacity: 0;
+    transition: all 0.8s ease-out 0.2s;
+  }
+`
+
+const StoryText = styled.div`
+  transform: translateY(40px);
+  opacity: 0;
+  transition: all 0.4s ease-out;
+  &:hover {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`
+
+const Divider = styled.hr`
+  width: 20%;
+  height: 0.2rem;
+`
+
+const Story = ({ picture, subTitle, title, children }) => {
+  const [state, setState] = useState({
+    isFullScreen: false,
+  });
+
+
+  return (
+    <Card isFullScreen={state.isFullScreen} >
+      <img className="story__picture" src={picture} alt="story-picture" />
+      <div className="story__content">
+        <div className="content__title">
+          <h4>{title}</h4>
+          <h2>{subTitle}</h2>
+          <Divider />
+        </div>
+        <div className="content__text">
+          <p>{children}</p>
+          <p>Read Story</p>
+        </div>
+      </div>
+    </Card>
+  )
+}
 
 const IndexPage = () => (
-  <Layout>
+  <Page>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="hero">
+      <Hero />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Main>
+      <Story className="track" subTitle="केरा" title="Day 1" picture={StoryPicture} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh sit amet
+        commodo nulla facilisi. Eget sit amet tellus cras adipiscing enim eu
+        turpis.
+      </Story>
+      <Story className="track" subTitle="केरा" title="Day 2" picture={StoryPicture} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh sit amet
+        commodo nulla facilisi. Eget sit amet tellus cras adipiscing enim eu
+        turpis.
+      </Story>
+      <Story className="track" subTitle="केरा" title="Day 3" picture={StoryPicture} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh sit amet
+        commodo nulla facilisi. Eget sit amet tellus cras adipiscing enim eu
+        turpis.
+      </Story>
+    </Main>
+    <footer style={{ background: "pink" }}></footer>
+  </Page>
 )
 
 export default IndexPage
